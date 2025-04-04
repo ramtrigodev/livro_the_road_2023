dc42b61f5f2cf530116f2639fc2bd1130d328866
Ramon Trigo <121834069+ramtrigodev@users.noreply.github.com>
2025-04-04 11:46:07 -0300

    criação lista

--- exemplo1/src/App.jsx
+++ exemplo1/src/App.jsx
@@ -1,12 +1,33 @@
 import * as React from 'react'
 
+const lista = [
+  {
+    titulo: 'React',
+    url: 'https://reactjs.org/',
+    autor: 'Ramon Trigo',
+    comentarios: 3,
+    pontos: 4,
+    id: 0,
+  },
+  {
+    titulo: 'React Native',
+    url: 'https://reactjs.org/',
+    autor: 'Pedro Muniz Trigo',
+    comentarios: 2,
+    pontos: 5,
+    id: 1,
+  },
+]
+
+
+
 const bemvindo = {
   saudacao: 'Oi',
   title: 'React',
 };
 
 function App() {
-  const title = 'React';
+ // const title = 'React';
   return (
     <div>
       <h1>
